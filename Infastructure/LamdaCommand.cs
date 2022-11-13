4b66713c72be6b6227fdf2fc6366c8dfaa9d2a49
Михаил Матвеев <107339955+MihailMatveev@users.noreply.github.com>
2022-11-13 11:31:05 +0300

    Создание класса LamdaCommand

---
+++ Infastructure/LamdaCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestWPF.Infastructure.Base;
+
+namespace TestWPF.Infastructure
+{
+    public class LamdaCommand:Command
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExectute;
+
+        public LamdaCommand(Action<object> Execute,Func<object,bool> CanExectute=null )
+        {
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            canExectute = CanExectute;
+        }
+        public override bool CanExecute(object parameter)=> canExectute?.Invoke(parameter)??true;
+        public override void Execute(object parameter) => execute(parameter);
+    }
+}
