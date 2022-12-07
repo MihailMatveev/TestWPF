55eebe4de0566d6f2900b2163cab358c967a07c3
Михаил Матвеев <107339955+MihailMatveev@users.noreply.github.com>
2022-12-07 19:34:21 +0300

    Создан класс User

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestWPF.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Password { get; set; }    
+
+    }
+}
