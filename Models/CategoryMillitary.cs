a9afab1dc647b2dde15babbfe5c5bef06f5f4e42
Михаил Матвеев <107339955+MihailMatveev@users.noreply.github.com>
2022-12-07 20:04:34 +0300

    Установил катеории в настройках

---
+++ Models/CategoryMillitary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestWPF.Models
+{
+    public class CategoryMillitary
+    {
+        public string Nuber { get; set; }
+        public string Name { get; set; }
+    }
+}
--- ViewModes/SettingVIewModel.cs
+++ ViewModes/SettingVIewModel.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TestWPF.Data;
+using TestWPF.Models;
 
 namespace TestWPF.ViewModes
 {
     public class SettingVIewModel:ViewModes.Base.ViewModelBase
     {
+        private List<CategoryMillitary> category;
+
+        public List<CategoryMillitary> Category
+        {
+            get { return category; }
+            set
+            {
+                Set(ref category, value);
+            }
+        }
+
+        public SettingVIewModel()
+        {
+            
+        }
     }
 }
